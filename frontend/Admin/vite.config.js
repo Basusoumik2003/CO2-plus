5cce6bf1375e22539ef3854376214149cfd91e2b
Amir faishal <136188380+amirfaishal@users.noreply.github.com>
2026-01-18 01:58:09 +0530

    Update vite.config.js

--- frontend/Admin/vite.config.js
+++ frontend/Admin/vite.config.js
@@ -40,7 +40,7 @@ export default defineConfig({
   },
   build: {
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
