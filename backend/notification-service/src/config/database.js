3a4e17e5cb233b5f43aa4d0db957881c051361ce
Amir faishal <136188380+amirfaishal@users.noreply.github.com>
2026-01-17 11:37:39 +0530

    Update database.js

--- backend/notification-service/src/config/database.js
+++ backend/notification-service/src/config/database.js
@@ -9,6 +9,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: String(process.env.DB_PASSWORD),
+  ssl: {
+    rejectUnauthorized: false
+  },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
