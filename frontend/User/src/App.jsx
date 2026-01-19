119588f9474af798214cf95f20c44beba12eee69
Amir faishal <136188380+amirfaishal@users.noreply.github.com>
2026-01-19 11:02:31 +0530

    Update App.jsx

--- frontend/User/src/App.jsx
+++ frontend/User/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation ,Navigate} from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
